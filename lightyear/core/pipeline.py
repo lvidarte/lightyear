bdcb981f64e2c1cdc3164b520de36677f59a7cba
Leo Vidarte <lvidarte@gmail.com>
2020-12-31 09:32:02 -0300

    Refactoring in Pipeline class

--- lightyear/core/pipeline.py
+++ lightyear/core/pipeline.py
@@ -16,10 +16,11 @@ class Pipeline:
         return get_logger(self._process_id(fn_name))
 
     def _process_id(self, fn_name):
-        name = ''
+        process_name = ''
         for process in self.config.pipeline:
             if process['function'] == fn_name:
-                name = process['name']
+                process_name = process['name']
+                break
         id_ = current_process()._identity[0]
-        return f"{name}-{id_}"
+        return f"{process_name}-{id_}"
 
