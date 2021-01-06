a23d48f8ee4c995a9cf94a8bed09bd802227e08c
Leo Vidarte <lvidarte@gmail.com>
2021-01-06 11:32:23 -0300

    Fix style

--- lightyear/core/logger.py
+++ lightyear/core/logger.py
@@ -1,32 +1,30 @@
 """Logger
 """
 
-import sys
 import logging
-
-from . import config
+import sys
 
 log_level = logging.DEBUG
+fmt = "[ %(levelname)s ] %(asctime)s | %(name)s (pid %(process)d) | %(message)s"
 
 
 def set_level(level):
     global log_level
     log_level = {
-        'error': logging.ERROR,
-        'warning': logging.WARNING,
-        'info': logging.INFO,
-        'debug': logging.DEBUG,
+        "error": logging.ERROR,
+        "warning": logging.WARNING,
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
     }[level]
 
 
-def get_logger(name='main-0', level=None):
+def get_logger(name="main-0", level=None):
     global log_level
     if level is None:
-        levle = log_level
+        level = log_level
     logger = logging.getLogger(name)
     handler = logging.StreamHandler(sys.stdout)
-    fmt = "[ %(levelname)s ] %(asctime)s | %(name)s (pid %(process)d) | %(message)s"
-    formatter = logging.Formatter(fmt, datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter(fmt, datefmt="%Y-%m-%d %H:%M:%S")
     handler.setFormatter(formatter)
     handler.setLevel(log_level)
     logger.addHandler(handler)
